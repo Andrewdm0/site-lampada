a7871ddd33c7de47ef1278473230712c79e85bc5
Andrewdmo <andrew.olivera@hotmail.com>
2021-08-05 17:09:57 -0300

    Função Apagar

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,5 @@
 function acender(){
-    let lamp = document.getElementById('lampada')
 
-    lamp.style.cssText = 'filter: drop-shadow(0 0 30px #e928f0) brightness(1.5);'
+        document.getElementById('lampada').classList.toggle('acesa');
     
 }
